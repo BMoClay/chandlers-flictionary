086f8b45ca47d7fd77edb4b7eb11d28dda693ac9
Chanson9892 <chan.hanson5@gmail.com>
2021-08-03 21:42:28 -0700

    made the webpage a sketchpad. want to add a clear feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState, useRef } from 'react';
 
 function App() {
+
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const [isDrawing, setIsDrawing] = useState(false)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    canvas.style.width = `${window.innerWidth}px`
+    canvas.style.height = `${window.innerHeight}px`
+
+    const context = canvas.getContext("2d")
+    context.scale(1,1)
+    context.lineCap = "round" // lines will have rounded edges
+    context.strokeStyle = "black" // line color
+    context.lineWidth = 5 // width of line
+    contextRef.current = context
+  }, [])
+
+  const startDrawing = ({nativeEvent}) => {
+    const {offsetX, offsetY} = nativeEvent
+    contextRef.current.beginPath()
+    contextRef.current.moveTo(offsetX,offsetY)
+    setIsDrawing(true)
+  }
+
+  const stopDrawing = () => {
+    contextRef.current.closePath()
+    setIsDrawing(false)
+  }
+
+  const draw = ({nativeEvent}) => {
+    if(isDrawing){
+      const {offsetX, offsetY} = nativeEvent
+      contextRef.current.lineTo(offsetX,offsetY)
+      contextRef.current.stroke()
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <canvas onMouseDown={startDrawing} onMouseUp={stopDrawing} onMouseMove={draw} ref={canvasRef}>
+
+    </canvas>
   );
 }
 
